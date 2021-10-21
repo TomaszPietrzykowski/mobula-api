e9b5ab0f20e00ddf6affc6eae1a9a9c219f556e9
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-10-21 03:31:08 +0200

    working setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
-import { AppProps } from 'next/app'
-import { Fragment } from 'react'
-import Footer from '../components/layout/Footer'
-import Header from '../components/layout/Header'
-import '../styles/globals.css'
+import { AppProps } from "next/app"
+import { Fragment } from "react"
+import Footer from "../components/layout/Footer"
+import Header from "../components/layout/Header"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <Fragment>
-    <section>
-    <Header title="Header title" color="green" />
-    <Component {...pageProps} />
-    </section>
-    <Footer />
-  </Fragment>
+    <Fragment>
+      <section>
+        <Header title="Mobula API" color="green" />
+        <Component {...pageProps} />
+      </section>
+      <Footer />
+    </Fragment>
   )
 }
 
--- pages/workspace.tsx
+++ pages/workspace.tsx
@@ -1,31 +1,69 @@
 import React, { FormEventHandler, useState } from "react"
 import styles from "../styles/Workspace.module.css"
+import axios from "axios"
 
 export interface KeyValuePair {
   key?: string
   value?: string
 }
+export interface RequestInterface {
+  url: string
+  method: any
+  params?: {}
+  headers?: {}
+}
 
 const Workspace = () => {
+  const [loading, setLoading] = useState<boolean>(false)
   const [activeTab, setActiveTab] = useState<string>("request")
   const [protocol, setProtocol] = useState<string>("http")
-  const [reqUrl, setReqUrl] = useState<string>("example.com")
+  const [reqUrl, setReqUrl] = useState<string>(
+    "jsonplaceholder.typicode.com/todos/1"
+  )
   const [reqHeaders, setReqHeaders] = useState<KeyValuePair[]>([])
-  const [reqQueries, setReqQueries] = useState<KeyValuePair[]>([
-    { key: "page", value: "1" },
-  ])
+  const [reqQueries, setReqQueries] = useState<KeyValuePair[]>([])
   const [reqMethod, setReqMethod] = useState<string>("GET")
   const [output, setOutput] = useState<string>("")
+  // axios request
+  interface makeRequest {
+    (p: string, url: string): Promise<any>
+  }
+  const makeRequest: makeRequest = async (p: string, url: string) => {
+    const outputHeaders = {}
+    reqHeaders.forEach((h) => {
+      outputHeaders[`${h.key}`] = h.value
+    })
+
+    const outputParams = {}
+    reqQueries.forEach((p) => {
+      outputParams[`${p.key}`] = p.value
+    })
+    const axiosConfig: RequestInterface = {
+      url: `${p}://${url}`,
+      method: reqMethod,
+      params: outputParams,
+      headers: outputHeaders,
+    }
+    console.log(axiosConfig.url)
+    setLoading(true)
+    try {
+      const { data } = await axios(axiosConfig)
+      setOutput(JSON.stringify(data))
+      setActiveTab("response")
+      setLoading(false)
+      return data
+    } catch (error) {
+      setOutput(JSON.stringify(error))
+      setActiveTab("response")
+      console.log(error)
+      setLoading(false)
+    }
+  }
   // form submit
   const handleSubmit = (e: React.SyntheticEvent): void => {
     e.preventDefault()
-    let queriesString: string =
-      reqQueries.length === 0
-        ? ""
-        : `?${reqQueries.map((q) => `${q.key}=${q.value}`).join("&")}`
-    const requestUrl: string = `${protocol}://${reqUrl}${queriesString}`
 
-    setOutput(`Method: ${reqMethod} URL: ${requestUrl}`)
+    makeRequest(protocol, reqUrl)
   }
   const handleProtocol = (e: any): void => {
     setProtocol(e.currentTarget.value)
@@ -43,7 +81,7 @@ const Workspace = () => {
   return (
     <div className={styles.root}>
       <h1>Workspace</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={styles.urlForm}>
         <select id="method" onChange={(e) => handleMethod(e)}>
           <option value="GET">GET</option>
           <option value="POST">POST</option>
@@ -52,14 +90,15 @@ const Workspace = () => {
           <option value="DELETE">DELETE</option>
         </select>
         <select id="protocol" onChange={(e) => handleProtocol(e)}>
-          <option value="http://">HTTP</option>
-          <option value="https://">HTTPS</option>
-          <option value="ftp://">FTP</option>
+          <option value="http">HTTP</option>
+          <option value="https">HTTPS</option>
+          <option value="ftp">FTP</option>
         </select>
         <input
           type="text"
           id="url"
           value={reqUrl}
+          className={styles.url}
           onChange={(e) => handleUrl(e)}
         />
         <button type="submit">Send</button>
@@ -86,13 +125,17 @@ const Workspace = () => {
           Response
         </label>
       </div>
-      {activeTab === "request" ? (
+      {loading ? (
+        <h1>Loading</h1>
+      ) : activeTab === "request" ? (
         <article className={styles.output}>
           <p>Request</p>
-          <p>{output}</p>
         </article>
       ) : (
-        <article className={styles.output}>response</article>
+        <article className={styles.output}>
+          <p>Response</p>
+          <p>{output}</p>
+        </article>
       )}
     </div>
   )
